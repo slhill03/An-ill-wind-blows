8754a02df18a1ec00d0c11cb8f3fa67c5f209599
Sam <47217896+slhill03@users.noreply.github.com>
2019-11-07 09:54:38 -0500

    Add files via upload

---
+++ This is (not) a Project Folder/main.lua
@@ -0,0 +1,16 @@
+function love.load()
+
+end
+
+
+function love.update()
+
+end
+
+
+function love.draw()
+love.graphics.setColor(0,0,1)
+love.graphics.rectangle("fill", 300, 200, 300, 200)
+love.graphics.setColor(1, 1, 1)
+love.graphics.rectangle("fill", 350, 250, 200, 100)
+end
